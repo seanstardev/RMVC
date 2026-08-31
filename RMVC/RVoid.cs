da839b68054024ed5897f508821b16c0da3e4d04
S <1278395+seanstardev@users.noreply.github.com>
2026-08-31 12:50:32 +0100

    Introduced RVoid.

---
+++ RMVC/RVoid.cs
@@ -0,0 +1,10 @@
+﻿public sealed class RVoid
+{
+    private RVoid()
+    {
+    }
+
+    public static RVoid Value { get; } = new RVoid();
+
+    public override string ToString() => nameof(RVoid);
+}
